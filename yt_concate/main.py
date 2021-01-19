16639d8f4ac1874840d5e7ea7581d576910591b2
Shiang Lin <49870928@stust.edu.tw>
2021-01-19 12:39:37 +0800

    test

--- yt_concate/main.py
+++ yt_concate/main.py
@@ -1,46 +1,41 @@
-resp = {"Employees": {{name: "a", "tel": "1234"}, {name: "b", "tel": "5678"}}}
+import urllib.request
+import json
+from .settings import api_key
 
-for i in resp["Employee"]:
-    print(i)
-
-
-# import urllib.request
-# import json
-
-# CHANNEL_ID = "UCKSVUHI9rbbkXhvAXK-2uxA"
+CHANNEL_ID = "UCKSVUHI9rbbkXhvAXK-2uxA"
 
 
-# def get_all_video_in_channel(channel_id):
-#     api_key = "AIzaSyC1ZvTwFRlEzKQ8trlzbCVtvahIMoGnRcc"
+def get_all_video_in_channel(channel_id):
+    api_key = "AIzaSyC1ZvTwFRlEzKQ8trlzbCVtvahIMoGnRcc"
 
-#     base_video_url = "https://www.youtube.com/watch?v="
-#     base_search_url = "https://www.googleapis.com/youtube/v3/search?"
+    base_video_url = "https://www.youtube.com/watch?v="
+    base_search_url = "https://www.googleapis.com/youtube/v3/search?"
 
-#     first_url = (
-#         base_search_url
-#         + "key={}&channelId={}&part=snippet,id&order=date&maxResults=25".format(
-#             api_key, channel_id
-#         )
-#     )
+    first_url = (
+        base_search_url
+        + "key={}&channelId={}&part=snippet,id&order=date&maxResults=25".format(
+            api_key, channel_id
+        )
+    )
 
-#     video_links = []
-#     url = first_url
+    video_links = []
+    url = first_url
 
-#     while True:
-#         inp = urllib.request.urlopen(url)
-#         resp = json.load(inp)
+    while True:
+        inp = urllib.request.urlopen(url)
+        resp = json.load(inp)
 
-#         for i in resp["items"]:
-#             if i["id"]["kind"] == "youtube#video":
-#                 video_links.append(base_video_url + i["id"]["videoId"])
+        for i in resp["items"]:
+            if i["id"]["kind"] == "youtube#video":
+                video_links.append(base_video_url + i["id"]["videoId"])
 
-#         try:
-#             next_page_token = resp["nextPageToken"]
-#             url = first_url + "&pageToken={}".format(next_page_token)
-#         except KeyError:
-#             break
-#     return video_links
+        try:
+            next_page_token = resp["nextPageToken"]
+            url = first_url + "&pageToken={}".format(next_page_token)
+        except KeyError:
+            break
+    return video_links
 
 
-# video_list = get_all_video_in_channel(CHANNEL_ID)
-# print(len(video_list))
+video_list = get_all_video_in_channel(CHANNEL_ID)
+print(len(video_list))
