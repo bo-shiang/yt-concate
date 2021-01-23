274c630b24b8d4aa15293a3f071c5920eee56344
Shiang Lin <49870928@stust.edu.tw>
2021-01-23 22:48:53 +0800

    change

--- yt_concate/main.py
+++ yt_concate/main.py
@@ -1,41 +1,44 @@
 import urllib.request
 import json
-from .API import API_KEY
+import sys
 
-# CHANNEL_ID = "UCKSVUHI9rbbkXhvAXK-2uxA"
-print(API_KEY)
+sys.path.append("/Users/Shiang/Desktop/yt-concate")
+from yt_concate.settings import SECRET_KEY
 
-# def get_all_video_in_channel(channel_id):
-#     api_key = "AIzaSyC1ZvTwFRlEzKQ8trlzbCVtvahIMoGnRcc"
-
-#     base_video_url = "https://www.youtube.com/watch?v="
-#     base_search_url = "https://www.googleapis.com/youtube/v3/search?"
-
-#     first_url = (
-#         base_search_url
-#         + "key={}&channelId={}&part=snippet,id&order=date&maxResults=25".format(
-#             api_key, channel_id
-#         )
-#     )
-
-#     video_links = []
-#     url = first_url
-
-#     while True:
-#         inp = urllib.request.urlopen(url)
-#         resp = json.load(inp)
-
-#         for i in resp["items"]:
-#             if i["id"]["kind"] == "youtube#video":
-#                 video_links.append(base_video_url + i["id"]["videoId"])
-
-#         try:
-#             next_page_token = resp["nextPageToken"]
-#             url = first_url + "&pageToken={}".format(next_page_token)
-#         except KeyError:
-#             break
-#     return video_links
+CHANNEL_ID = "UCKSVUHI9rbbkXhvAXK-2uxA"
 
 
-# video_list = get_all_video_in_channel(CHANNEL_ID)
-# print(len(video_list))
+def get_all_video_in_channel(channel_id):
+    api_key = SECRET_KEY
+
+    base_video_url = "https://www.youtube.com/watch?v="
+    base_search_url = "https://www.googleapis.com/youtube/v3/search?"
+
+    first_url = (
+        base_search_url
+        + "key={}&channelId={}&part=snippet,id&order=date&maxResults=25".format(
+            api_key, channel_id
+        )
+    )
+
+    video_links = []
+    url = first_url
+
+    while True:
+        inp = urllib.request.urlopen(url)
+        resp = json.load(inp)
+
+        for i in resp["items"]:
+            if i["id"]["kind"] == "youtube#video":
+                video_links.append(base_video_url + i["id"]["videoId"])
+
+        try:
+            next_page_token = resp["nextPageToken"]
+            url = first_url + "&pageToken={}".format(next_page_token)
+        except KeyError:
+            break
+    return video_links
+
+
+video_list = get_all_video_in_channel(CHANNEL_ID)
+print(len(video_list))
