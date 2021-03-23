1dc8d601622a8a0e4e60a75beb67c42db398aaaf
Shiang Lin <49870928@stust.edu.tw>
2021-03-23 22:35:06 +0800

    test

--- yt_concate/pipeline/steps/download_caption.py
+++ yt_concate/pipeline/steps/download_caption.py
@@ -1,16 +1,28 @@
 from pytube import YouTube
 from yt_concate.pipeline.steps.step import Step
 from yt_concate.pipeline.steps.step import StepException
+import time
 
 
 class DownloadCaptions(Step):
-    def process(self, data, inputs):
+    def process(self, data, inputs, utils):
+        start = time.time()
         for url in data:
+            print('downloading caption for', url)
+            if utils.caption_file_exists(url):
+                print("found exiting caption file")
+                continue
+            try:
             yt = YouTube(url)
             caption = yt.captions.get_by_language_code("a.en")
-            print(caption.generate_srt_captions())
-            save the caption to a file named Output.txt
-            text_file = open("Output.txt", "w")
-            text_file.write(caption_convert_to_srt)
+            # print(caption.generate_srt_captions())
+            # save the caption to a file named Output.txt
+            except (KeyError, AssertionError):
+                print('KeyError when downloading caption for', url)
+                continue
+            text_file = open(utils.get_caption_path(url), "w", encoding="utf-8")
+            text_file.write(caption.generate_srt_captions())
             text_file.close()
-            break
+
+        end = time.time()
+        print("took", end - start, "seconds")
--- yt_concate/settings.py
+++ yt_concate/settings.py
@@ -2,5 +2,9 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-
 SECRET_KEY = os.getenv("SECRET_KEY")
+
+VIDEO_LIST_FILENAME = "video_list.txt"
+DOWNLOADS_DIR = "downloads"
+VIDEOS_DIR = os.path.join(DOWNLOADS_DIR, "videos")
+CAPTIONS_DIR = os.path.join(DOWNLOADS_DIR, "captions")
